c7e20c9f1cf84087e1bc1120333dc3e5c0a4025e
AnggiIllahi <anggisulton@gmail.com>
2017-03-02 16:01:15 +0700

    Commit Pertama

---
+++ application/views/About.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Biodata Diri</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/bootstrap.min.css" >
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+	<div class="container">
+		<div class="col-xs-17 col-sm-17 col-md-17 col-lg-17">
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Biodata Diri</a>
+					</div>
+			
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+						<ul class="nav navbar-nav">
+							<li><a href="<?php echo site_url() ?>/Home">Home</a></li>
+							<li class="active"><a href="<?php echo site_url() ?>/About">About</a></li>
+							<li><a href="<?php echo site_url() ?>/Contact">Contact</a></li>
+						</ul>
+						
+						<ul class="nav navbar-nav navbar-right">
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div>
+			</nav>
+
+			
+
+			<div class="jumbotron">
+
+			<table border=0 class="table" width=90% cellpadding="0">
+				<tr>
+					<td rowspan=6 align="center">
+						<img src="<?php echo base_url() ?>img/Anggi.jpg" class="img-responsive" alt="Image" width=270>
+					</td>
+					
+				</tr>
+				<tr>
+					<td>
+						<div class="alert alert-success" role="alert">Nama : Anggi Illahi Sulton</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="alert alert-info" role="alert">NIM : 1541180003</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="alert alert-warning" role="alert">Alamat : RT28 RW14 Desa Sidorejo Pepe, Kecamatan Rowokangkung, Kabupaten Lumajang</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="alert alert-danger" role="alert">No Tlp : 081615747687</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div class="alert alert-success" role="alert">Hobby : Fotografi</div>
+					</td>
+				</tr>
+			</table>		
+			</div>
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<hr align=center size=3 width=90%>
+					<font size=2 color=black face="century gothic" ><center> 
+						Copyright &copy 2017<b>&nbsp Pemrograman Framework TI-2A</b></a><br>
+						Prodi D4 Teknik Informatika | Jurusan Teknologi Informasi<br><b>POLITEKNIK NEGERI MALANG</b></a>
+						<br><br>
+					</center></font>
+			</div>
+		</div>
+	</div>
+		
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
